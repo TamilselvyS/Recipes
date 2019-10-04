89e5ddedc458e5b3a3d5fc3468b396cf8914fd3e
tamilselvysm23 <tamilselvysm23@gmail.com>
2019-10-04 20:30:02 +0530

    API

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 const url = require('url');
- //creating a server
- const server = http.createServer ((req, res)=>{
+
+
+//Json file
+const data = fs.readFileSync(`${__dirname}/recipes.json`, 'utf-8');
+const dataObject = JSON.parse(data);
+
+//creating a server
+const server = http.createServer((req, res) => {
     const pathName = req.url;
-    if(pathName === '/' || pathName === '/home'){
+    if (pathName === '/' || pathName === '/home') {
         res.end("This is home!");
-    }else if(pathName === '/recipe'){
+    } else if (pathName === '/recipe') {
         res.end("This is recipe!");
- }
- else{
-    res.writeHead(404, {
-        'Content-type' : 'text/html',
-        'Own-header' : 'Hello'
-    });
-    res.end('<h1>Page Not Found!!</h1>');
-}
+    }
+    else if (pathName === '/api') {
+        res.writeHead(200, { 'Content-type': 'application/json' });
+        res.end(data);
+    }
+    else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'Own-header': 'Hello'
+        });
+        res.end('<h1>Page Not Found!!</h1>');
+    }
 
 });
- //starting the server
- server.listen(8000, '127.0.0.1', ()=>{
-     console.log('Listing to request on port 8000');
- });
+//starting the server
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listing to request on port 8000');
+});
